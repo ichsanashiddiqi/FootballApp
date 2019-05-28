4dceb93dd1e20b1ee820d0ccba509cc792d153b6
debbysa <deby.silvia23@gmail.com>
2019-05-28 09:19:21 +0700

    fix tampilan match

--- app/src/main/java/com/ichsan/Footballapp/ui/MainActivity.java
+++ app/src/main/java/com/ichsan/Footballapp/ui/MainActivity.java
@@ -7,14 +7,17 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ichsan.Footballapp.R;
 import com.ichsan.Footballapp.ui.competitions.CompetitionsActivity;
 import com.ichsan.Footballapp.ui.matches.FixturesActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -38,14 +41,14 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (id) {
                     case navCompetitions:
-                        Log.d(LOG_TAG, "Competitions tab selected");
+                        Toast.makeText(MainActivity.this, "Competitions clicked!",Toast.LENGTH_SHORT).show();
                         toolbar.setTitle(R.string.navigation_competitions);
                         fragment = new CompetitionsActivity();
                         loadFragment(fragment);
                         break;
                     case navFixtures:
                     default:
-                        Log.d(LOG_TAG, " Match tab selected");
+                        Toast.makeText(MainActivity.this, "Match clicked!",Toast.LENGTH_SHORT).show();
                         toolbar.setTitle(R.string.navigation_fixtures);
                         fragment = new FixturesActivity();
                         loadFragment(fragment);
@@ -64,4 +67,17 @@ public class MainActivity extends AppCompatActivity {
                 .addToBackStack(null)
                 .commit();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+//        final MenuItem searchItem = menu.findItem(R.id.action_search);
+//        final SearchView searchView = (SearchView) searchItem.getActionView();
+//        searchView.setOnQueryTextListener((SearchView.OnQueryTextListener) this);
+
+        return true;
+    }
+
 }
--- app/src/main/java/com/ichsan/Footballapp/ui/competitions/CompetitionsActivity.java
+++ app/src/main/java/com/ichsan/Footballapp/ui/competitions/CompetitionsActivity.java
@@ -8,11 +8,16 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import com.ichsan.Footballapp.R;
 import com.ichsan.Footballapp.data.model.competitions.Competition;
@@ -33,6 +38,7 @@ public class CompetitionsActivity extends Fragment implements ListItemClickListe
 
     private static final String LOG_TAG = CompetitionsActivity.class.getSimpleName();
     private List<Competition> competitionList;
+    private EditText search;
 
     public CompetitionsActivity() {
 
@@ -45,6 +51,29 @@ public class CompetitionsActivity extends Fragment implements ListItemClickListe
         Log.d(LOG_TAG, LOG_TAG + " ACTIVE");
 
         RecyclerView list = view.findViewById(R.id.list_competitions);
+
+        //search
+//        final MenuItem searchItem = menu.findItem(R.id.action_search);
+//        search = view.findViewById(R.id.action_search);
+//        search.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                if (count != 0) {
+//                    cariCompetions(search.getText().toString(), view);
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
         list.setLayoutManager(layoutManager);
@@ -98,13 +127,64 @@ public class CompetitionsActivity extends Fragment implements ListItemClickListe
 
     @Override
     public void onListItemClick(int clickedItemIndex, int clickedItemId, String clickedItemName) {
-        String msg = "Item #" + clickedItemIndex + " [" + clickedItemName + "] with id of " + clickedItemId + " clicked.";
-        Log.d(LOG_TAG, msg);
-        Toast.makeText(this.getActivity(), msg, Toast.LENGTH_LONG).show();
 
         Intent intent = new Intent(this.getActivity(), CompetitionActivity.class);
         intent.putExtra("competitionId", clickedItemId);
         intent.putExtra("competitionName", clickedItemName);
         startActivity(intent);
     }
+
+
+//    private void cariCompetions(String cari, View view) {
+//        RecyclerView list = view.findViewById(R.id.list_competitions);
+//
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
+//        list.setLayoutManager(layoutManager);
+//        list.setHasFixedSize(true);
+//        list.setItemAnimator(new DefaultItemAnimator());
+//
+//        ApiService apiService = ApiClient.getClient().create(ApiService.class);
+//
+//        Call<Competitions> call = apiService.getCompetitions(cari);
+//        call.enqueue(new Callback<Competitions>() {
+//            @Override
+//            public void onResponse(@NonNull Call<Competitions> call, @NonNull final Response<Competitions> response) {
+//                if (!response.isSuccessful()) {
+//                    Log.e(LOG_TAG, "FAILED CONNECTION with code " + response.code() + ": " + response.errorBody());
+//                    return;
+//                }
+//
+//                Log.d(LOG_TAG, "SUCCESSFUL CONNECTION with code " + response.code());
+//
+//                final Competitions res = response.body();
+//
+//                if (res != null) {
+//                    Log.d(LOG_TAG, "Data received from " + response.body().toString());
+//
+//                    competitionList = new ArrayList<>(res.getCompetitionList());
+//                    Log.d(LOG_TAG, "competitionList:" + competitionList.size());
+//
+//                    CompetitionsAdapter adapter = new CompetitionsAdapter(competitionList, CompetitionsActivity.this);
+//                    list.setAdapter(adapter);
+//
+//                    DividerItemDecoration divider = new DividerItemDecoration(list.getContext(), layoutManager.getOrientation());
+//                    list.addItemDecoration(divider);
+//                } else {
+//                    Log.w(LOG_TAG, "RESPONSE IS NULL!");
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<Competitions> call, @NonNull Throwable t) {
+//                try {
+//                    Log.e(LOG_TAG, "QUERY FAILED: " + t.toString() + " >>> CAUSED BY: " + t.getCause());
+//                    throw t;
+//                } catch (Throwable throwable) {
+//                    throwable.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+
 }
+
